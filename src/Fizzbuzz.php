803457347611eb93a980f5a04cb88b5867d8b76f
Enrique Segura <enrique.segura@hyve.net>
2016-09-11 17:23:44 +0200

    Finished all the test and all the test done.

--- src/Fizzbuzz.php
+++ src/Fizzbuzz.php
@@ -1,10 +1,70 @@
 <?php
 
-
 namespace EnriqueSegura;
-
 
 class Fizzbuzz
 {
+    private $fizz = 'Fizz';
+    private $buzz = 'Buzz';
+    private $fizzBuzz = 'FizzBuzz';
 
+
+    public function output($number)
+    {
+        $result = "";
+
+        for($i=1;$i<=$number;$i++){
+            if(($this->isNumMultipleOfThree($i)) && ($this->isNumMultipleOfFive($i))){
+                $result .= $this->getFizzBuzz();
+            }elseif ($this->isNumMultipleOfThree($i)){
+                $result .= $this->getFizz();
+            }elseif ($this->isNumMultipleOfFive($i)){
+                $result .= $this->getBuzz();
+            }else{
+                $result .= $i;
+            }
+            $result .= "\n";
+        }
+        return $result;
+    }
+
+    public function setFizz($string)
+    {
+        $this->fizz = $string;
+    }
+
+    public function setBuzz($string)
+    {
+        $this->buzz = $string;
+    }
+
+    public function setFizzBuzz($string)
+    {
+        $this->fizzBuzz = $string;
+    }
+
+    private function getFizzBuzz()
+    {
+        return $this->fizzBuzz;
+    }
+
+    private function getFizz()
+    {
+        return $this->fizz;
+    }
+
+    private function getBuzz()
+    {
+        return $this->buzz;
+    }
+
+    private function isNumMultipleOfThree($number)
+    {
+        return $number % 3 == 0;
+    }
+
+    private function isNumMultipleOfFive($number)
+    {
+        return $number % 5 == 0;
+    }
 }
--- test/FizzbuzzTest.php
+++ test/FizzbuzzTest.php
@@ -4,8 +4,20 @@ use EnriqueSegura\Fizzbuzz;
 
 class FizzbuzzTest extends PHPUnit_Framework_TestCase
 {
-    public function testMock()
-     {
-         $this->assertInstanceOf('\EnriqueSegura\FizzBuzz', new Fizzbuzz);
-     }
+    public function testOutputNumberWhenNotMultipleOfThreeOrFive()
+    {
+        $fizzbuzz =  new Fizzbuzz();
+        $this->assertEquals("1\n2\n",$fizzbuzz->output(2));
+    }
+
+    public function testOutputNumberWhenMultipleOfThreeAndFive()
+    {
+        $fizzbuzz = new Fizzbuzz();
+
+        $fizzbuzz->setFizz("three");
+        $fizzbuzz->setBuzz("five");
+        $fizzbuzz->setFizzBuzz("both");
+
+        $this->assertEquals("1\n2\nthree\n4\nfive\nthree\n7\n8\nthree\nfive\n11\nthree\n13\n14\nboth\n16\n", $fizzbuzz->output(16));
+    }
 }
